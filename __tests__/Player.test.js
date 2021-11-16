3ad9672ea0c115332abc6bd4e9a7eb2324a4e69b
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-11-15 23:57:14 -0700

    added potion

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -40,7 +40,7 @@ test("gets player's health value", () => {
     const player = new Player('Dave');
     // method is an expect method that we can use to make sure our string includes our player's health
     expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
-  });
+});
 
 test('checks if player is alive or not', () => {
     const player = new Player('Dave');
@@ -63,4 +63,22 @@ test("subtracts from player's health", () => {
     player.reduceHealth(99999);
   
     expect(player.health).toBe(0);
+});
+
+test("gets player's attack value", () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+  
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test('uses a potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+  
+    player.usePotion(1);
+  
+    expect(player.inventory.length).toBeLessThan(oldCount);
   });
